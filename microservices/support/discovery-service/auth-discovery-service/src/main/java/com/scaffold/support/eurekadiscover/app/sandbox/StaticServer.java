f40cb9e4f12d6cc20cde44c7f12eef8d0f26c59e
mj <mj@mj.com>
2016-02-02 22:30:46 +0100

    unbelievable

--- microservices/support/discovery-service/auth-discovery-service/src/main/java/com/scaffold/support/eurekadiscover/app/sandbox/StaticServer.java
+++ microservices/support/discovery-service/auth-discovery-service/src/main/java/com/scaffold/support/eurekadiscover/app/sandbox/StaticServer.java
@@ -12,9 +12,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import org.reactivestreams.Publisher;
+import org.reactivestreams.Subscriber;
 import org.springframework.stereotype.Component;
 
 import reactor.core.publisher.EmitterProcessor;
+import reactor.core.publisher.Mono;
 import reactor.core.subscriber.ConsumerSubscriber;
 import reactor.io.net.preprocessor.CodecPreprocessor;
 import reactor.rx.Broadcaster;
@@ -59,17 +62,17 @@ public class StaticServer extends AbstractVerticle {
 	}
 
 	ReactorTcpClient<Pojo, Pojo> client;
-
+	ChannelStream<Pojo, Pojo> ch2 = null;
 	public void reactor() throws InterruptedException {
 		final int port = 1234;// SocketUtils.findAvailableTcpPort();
 
 		final CountDownLatch latch = new CountDownLatch(1);
-		client = NetStreams.tcpClient(s -> s.connect("localhost", port)
+		client = NetStreams.tcpClient(s -> s.connect("localhost", 1234)
 				.preprocessor(CodecPreprocessor.json(Pojo.class)));
 
 		final ReactorTcpServer<Pojo, Pojo> server = NetStreams.tcpServer(s -> s.listen("localhost", port).preprocessor(
 				CodecPreprocessor.json(Pojo.class)));
-		EmitterProcessor<Pojo> create = EmitterProcessor.create();
+		
 		server.start(input ->
 
 		// for each connection echo any incoming data
@@ -88,38 +91,85 @@ public class StaticServer extends AbstractVerticle {
 
 		.doOnNext(p -> {
 			if (p.getName().equals("working")) {
-				create.onNext(new Pojo("bbbb"));
 				System.out.println("tutaj");
 			}
-			System.out.println(p.getName());
+			System.out.println("RECEIVED:"+p.getName());
 		}).map(i -> new Pojo(i.getName().toUpperCase()))
 
-		.capacity(1l) // auto-flush every 5 elements
+		.capacity(10l) // auto-flush every 5 elements
 
 		)
 
 		);
 		
 		Broadcaster<Pojo> b = Broadcaster.create();
-		// create.subscribe(b);
+//		Broadcaster<Pojo> sink = Broadcaster.create();
+//		b.doOnNext(po -> {
+//			System.out.println(po);
+//		}).consume();
+		EmitterProcessor<Pojo> create = EmitterProcessor.create();
+//		 create.subscribe(b);
 		 create.start();
-
+//		 Publisher<Pojo> p = create;
+		 
+//		 create.onNext(new Pojo("to jest to11"));
+//		 b.onNext(new Pojo("fsdfadfa"));
+		 
 		client.start(ch -> {
-			ch.subscribe(create);
-			return ch.writeWith(create.doOnNext(n -> {
-				System.out.println(n);
-				ch.writeWith(Stream.just(n));
-			}));
-		}).get();
-
-		b.onNext(new Pojo("aaaa"));
-		create.onNext(new Pojo("bbbb"));
-		// b.onNext(new Pojo("fsdfadfa"));
+			b.consume(po -> {
+				System.out.println(po);
+//				ch.writeWith(Stream.just(po));
+				Stream.from(ch.writeWith(Stream.just(po))).consume();
+			});
+//			ch2 = ch;
+//			ch2.subscribe(create);
+//			Stream.from(ch2.writeWith(create)).consume();
+			return Stream.never();
+		}).doOnTerminate((i, t) -> {
+			System.out.println("ter"+i + ","+ t);
+//			Stream.from(ch2.writeWith(Stream.just(new Pojo("suu")))).consume();
+		})
+		.doOnSubscribe(s -> {
+			System.out.println("subscrie"+s);
+//			Stream.from(ch2.writeWith(Stream.just(new Pojo("suu")))).consume();
+		})
+		.doOnSuccess(s -> {
+			System.out.println("success"+s);
+//			Stream.from(ch2.writeWith(Stream.just(new Pojo("sss")))).consume();
+		}).subscribe();
+		
+//		b.startWith(new Pojo("fsad"));
+//		Thread.sleep(3000);
+//		Stream.from(ch2.writeWith(Stream.just(new Pojo("dfasfa")))).consume();
+//		Stream.from(ch2.writeWith(Stream.just(new Pojo("yyyy")))).consume();
+//		Stream.from(ch2.writeWith(Stream.empty())).consume();
+//		Stream.from(ch2.writeWith(create)).consume();
+//		ch2.subscribe(create);
+//		ch2.writeWith(b);
+//		ch2.writeWith(create);
+//		client.start(ch -> {
+//			
+//			System.out.println(ch);
+//			return ch.writeWith(Stream.just(new Pojo("3332")));
+//		}).subscribe();
+//		sink.onNext(new Pojo("aaaa"));
+//		create.onNext(new Pojo("to jest to"));
+		create.doOnNext(po -> {
+			System.out.println(po);
+		});
+//		b.consume(po -> {
+//			System.out.println("B:"+po);
+//		});
+		create.onNext(new Pojo("00000"));
+		create.onNext(new Pojo("1111111"));
+		 b.onNext(new Pojo("22222"));
+		 b.onNext(new Pojo("33333"));
+		 create.onNext(new Pojo("44444"));
 		// broadcast.onNext(new Pojo("at last"));
 		//
 		//
 		// client.shutdown();
-		// latch.await(10, TimeUnit.SECONDS);
+//		 latch.await(30, TimeUnit.SECONDS);
 		// server.shutdown();
 	}
 
